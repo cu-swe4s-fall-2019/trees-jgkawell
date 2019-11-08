afaa3c7bc5e30a7d096b8bf7c7811284fc9e0f4f
Jack Kawell <jgkawell@outlook.com>
2019-11-07 22:37:22 -0700

    Added tests for searching

--- test_binary_tree.py
+++ test_binary_tree.py
@@ -35,6 +35,30 @@ class TestBinaryTree(unittest.TestCase):
         self.assertEqual(
             str(ex.exception), "Keys must be integers")
 
+    def test_random_insert_and_search(self):
+        tree = binary_tree.BinaryTree(1, 'a')
+
+        entries = {}
+        for i in range(0, 100):
+            key = random.randint(-1000,1000)
+            value = random.randint(-1000,1000)
+            tree.insert(key, value)
+
+        for key, value in entries.items():
+            self.assertEqual(tree.search(key), value)
+
+    def test_non_num_key_on_search(self):
+        tree = binary_tree.BinaryTree(10, 'a')
+        tree.insert(15, 'b')
+        tree.insert(5, 'c')
+        tree.insert(12, 'd')
+
+        with self.assertRaises(TypeError) as ex:
+            tree.search("not int")
+
+        self.assertEqual(
+            str(ex.exception), "Keys must be integers")
+
 
 if __name__ == '__main__':
     unittest.main()
